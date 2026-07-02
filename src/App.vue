de0c1f5bb168666d5cb2977e40ed2b6a9f25a9d4
Cristian <cchura.cpc@gmail.com>
2026-07-01 20:27:34 -0400

    CRUD Productos

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <RouterLink to="/">INICIO</RouterLink> |
+  <RouterLink to="/producto">PRODUCTOS</RouterLink> |
+  <RouterLink to="/login">INGRESAR</RouterLink>
+
+  <RouterView />
+
+</template>
---
+++ src/views/Producto.vue
@@ -0,0 +1,95 @@
+<template>
+    <h1>Nuestros Productos</h1>
+    <!--
+    <h3>El Producto: {{ producto }}</h3>
+    <h3>Lista de Productos:</h3>{{ productos }}
+-->
+    <div>
+        <label for="">Ingrese Nombre: </label>
+        <input type="text" v-model="producto.nombre">
+        <br>
+        <label for="">Ingrese Stock: </label>
+        <input type="number" v-model="producto.stock">
+        <br>
+        <label for="">Ingrese Precio: </label>
+        <input type="number" v-model="producto.precio">
+        <br>
+        <label for="">Ingrese Imagen: </label>
+        <input type="text" v-model="producto.imagen">
+        <br>
+        <button @click="funGuardarProducto()">Guardar Producto</button>
+    </div>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>NOMBRE</th>
+                <th>STOCK</th>
+                <th>PRECIO</th>
+                <th>IMAGEN</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(prod, posicion) in productos">
+                <td>{{ prod.nombre }}</td>
+                <td>{{ prod.stock }}</td>
+                <td>{{ prod.precio }}</td>
+                <td>
+                    <img :src="prod.imagen" alt="" width="100">
+                    
+                </td>
+                <td>
+                    <button @click="funEditar(prod)">editar</button>
+                    <button @click="funEliminar(posicion)">eliminar</button>
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
+
+</template>
+
+<script setup>
+    import { onMounted, ref } from 'vue'
+    
+    const productos = ref([]);
+    const producto = ref({});
+    const edicion = ref(false)
+
+    onMounted(() => {
+        // alert("Cargando Producto...")
+        funListar()
+    });
+
+    function funListar(){
+        const datos = localStorage.getItem("productos") || "[]";
+        productos.value = JSON.parse(datos);
+    }
+
+    function funGuardarProducto(){
+        if(edicion.value){
+            producto.value = {}
+            edicion.value = false;
+        }else{
+            const nuevoProd = producto.value
+            productos.value.push(nuevoProd);
+            producto.value = {}
+
+        }
+        localStorage.setItem("productos", JSON.stringify(productos.value))
+        
+    }
+
+    function funEditar(prod){
+        edicion.value = true;
+        producto.value = prod;
+        
+    }
+
+    function funEliminar(posicion){
+        productos.value.splice(posicion, 1);
+    }
+
+
+</script>
