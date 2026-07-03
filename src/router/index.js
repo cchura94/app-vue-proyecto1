473f6d24de339529f5a0d3f4cd3e95e158d7fe5c
Cristian <cchura.cpc@gmail.com>
2026-07-02 20:41:59 -0400

    actuaizacion

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Producto from "./../views/Producto.vue"
 import Login from "./../views/Login.vue"
 import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 import Blog from "../views/Blog.vue";
+import NotFoundComponent from "../views/errors/NotFoundComponent.vue";
 
 const mis_rutas = [
     {
@@ -20,10 +21,11 @@ const mis_rutas = [
     {
         path: '/blog',
         component: Blog
-    }
+    },
+    { path: '/:pathMatch(.*)', component: NotFoundComponent }
 ];
 
 export const router = createRouter({
-    history: createWebHashHistory('/app-vue-proyecto1/'),
+    history: createWebHistory('/app-vue-proyecto1/'),
     routes: mis_rutas
 })
